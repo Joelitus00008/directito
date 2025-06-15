b8392e30c30e01f06f5ab91cf231690329a58a46
Joelitus00008 <rubionuez@gmail.com>
2025-06-15 03:29:58 +0200

    Delete script - copia.js

--- script - copia.js
+++
@@ -1,6 +0,0 @@
-function startTimer() {
-  setTimeout(() => {
-    document.getElementById('player').style.display = 'none';
-    document.getElementById('pago').style.display = 'block';
-  }, 30 * 60 * 1000);
-}
